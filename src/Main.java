c783149f962aaedd46db3c184870c83e889898b6
Guilherme <ariel.gmachado@hotmail.com>
2025-06-07 15:46:58 -0300

    Fist commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao Scree Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2025;
+        System.out.println("Ano de lançamento: " + ano);
+
+    }
+}
