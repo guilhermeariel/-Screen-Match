538c5aa7352c8b7425ceb2f112b4690ce8e1fcc8
Guilherme <ariel.gmachado@hotmail.com>
2025-06-07 19:56:01 -0300

    Subindo

---
+++ src/Condicionais.java
@@ -0,0 +1,20 @@
+public class Condicionais {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1986;
+        boolean incluidoNoPlano =  false;
+        double notaDoFilme = 9;
+        String tipoPlano = "normal";
+
+        if (anoDeLancamento >= 1986){
+            System.out.println("Lançamento! ");
+        } else {
+            System.out.println("Filme retro");
+        }
+
+        if (incluidoNoPlano || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao Scree Match");
+        System.out.println("Bem vindo ao Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
 
         int ano = 2025;
         System.out.println("Ano de lançamento: " + ano);
+        boolean incluidoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (8 + 6.81 + 10) /3;
+        // Média calculada pelas 3 notas no Globo Rewards
+        String formatado =  String.format("%.2f", media);
+        System.out.println("Nota do filme: " + formatado);
+        String sinopse = """ 
+                Filme de ação com o Brabo
+                Ano de lançamento
+                """ + ano;
+        System.out.println(sinopse);
+
+        float classificacao = (float) (media /2);
+        System.out.println(String.format("%.2f", classificacao));
+
+
+
+
+//        String nome = "Maria";
+//        int idade = 30;
+//        double valor = 55.9999;
+//        System.out.println(String.format("Meu nome é %s, eu tenho %d anos e hoje gastei %.2f reais", nome, idade, valor));
+
+
+
 
     }
 }
